073a2aaa1633ddf6591bfad16edf1a944cbaafae
MarcellusStack <marcellusstack@gmail.com>
2024-05-04 16:10:34 +0200

    update

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,13 @@
 import Link from "next/link";
 
 export const runtime = "edge";
+export const revalidate = 30;
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       Test <Link href="/test">Test Revalidate Page</Link>
+      {new Date().toISOString()}
     </main>
   );
 }
